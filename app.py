0e32b2f494d9484ca00a72d7cc0ca00e58c53f53
Pothiraj K <121817158+POTHI016@users.noreply.github.com>
2026-03-03 16:16:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,6 @@ def receive():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5003))
     app.run(host="0.0.0.0", port=port)
+
